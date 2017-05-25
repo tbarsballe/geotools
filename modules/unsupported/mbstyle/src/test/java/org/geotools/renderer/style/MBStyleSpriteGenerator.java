011b6cc2989bcc29ade07d6c1e636cb9df8a4c5c
mattkrusz <mkruszewski@boundlessgeo.com>
2017-05-25 10:11:06 -0500

    MBStyle - Additions to MBStyleSpriteGenerator
    - Now includes ShapeMarkFactory shapes (prefix must be added in sprite index file)
    - Now draws the shapes with default black / gray styling
    - Saves them all to mark-sprite-output resources folder
    - Changed default scale to 32
    - Ensured all shapes have GeoTools default orientation (star, triangle, slash, backslash were 180* rotated)

--- modules/unsupported/mbstyle/src/test/java/org/geotools/renderer/style/MBStyleSpriteGenerator.java
+++ modules/unsupported/mbstyle/src/test/java/org/geotools/renderer/style/MBStyleSpriteGenerator.java
@@ -5,32 +5,68 @@ import org.apache.batik.svggen.SVGGraphics2D;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map.Entry;
 
 /**
  * Utility class for generating SVG icons from SLD marks
  *
  * To convert from SVG icons to MapBox sprites, use spritezero: https://github.com/mapbox/spritezero-cli
+ * 
+ * Note: Shapes from other MarkFactories (aside from {@link WellKnownMarkFactory} often have a prefix
+ * like "shape://". Those prefixes are not included in the filenames written to disk, because they may be invalid.
+ * When using the files to generate the sprite index file, they should be renamed to have the correct prefix.
+ * 
  */
 public class MBStyleSpriteGenerator {
 
-    public static void main(String[] args) throws IOException {
+    private static final int SCALE = 32;
 
-        generateSVG("cross-24.svg", WellKnownMarkFactory.cross);
-        generateSVG("star-24.svg", WellKnownMarkFactory.star);
-        generateSVG("triangle-24.svg", WellKnownMarkFactory.triangle);
-        generateSVG("arrow-24.svg", WellKnownMarkFactory.arrow);
-        generateSVG("X-24.svg", WellKnownMarkFactory.X);
-        generateSVG("hatch-24.svg", WellKnownMarkFactory.hatch);
-        generateSVG("square-24.svg", WellKnownMarkFactory.square);
-        generateSVG("circle-24.svg", WellKnownMarkFactory.circle);
+    private static Path OUTPUT_FOLDER = Paths.get("src/test/resources/mark-sprite-output");
+    
+    public static void main(String[] args) throws IOException, URISyntaxException {
+       
+        // Generate WellKnownMarkFactory marks
+        generateSVG("cross.svg", WellKnownMarkFactory.cross);
+        generateSVG("star.svg", WellKnownMarkFactory.star, Math.PI);
+        generateSVG("triangle.svg", WellKnownMarkFactory.triangle, Math.PI);
+        generateSVG("arrow.svg", WellKnownMarkFactory.arrow);
+        generateSVG("X.svg", WellKnownMarkFactory.X);
+        generateSVG("hatch.svg", WellKnownMarkFactory.hatch, Math.PI);
+        generateSVG("square.svg", WellKnownMarkFactory.square);
+        generateSVG("circle.svg", WellKnownMarkFactory.circle);
+
+        // Generate ShapeMarkFactory marks
+        for (Entry<String, Shape> entry : ShapeMarkFactory.shapes.entrySet()) {
+            // Rotate certain shapes so the output matches default GeoTools output.
+            if (entry.getKey().trim().equals("slash") || entry.getKey().trim().equals("backslash")) {
+                generateSVG("ShapeMarkFactory-" + entry.getKey() + ".svg", entry.getValue(),
+                        Math.PI / 2.0);
+            } else {
+
+                generateSVG("ShapeMarkFactory-" + entry.getKey() + ".svg", entry.getValue());
+            }
+        }
     }
 
-    public static void generateSVG(String filename, Shape shape) throws IOException {
+    public static void generateSVG(String filename, Shape shape) throws IOException, URISyntaxException {
+        generateSVG(filename, shape, null);
+    }
+
+    public static void generateSVG(String filename, Shape shape, Double rotation)
+            throws IOException, URISyntaxException {
         // Get a DOMImplementation.
         DOMImplementation domImpl = GenericDOMImplementation.getDOMImplementation();
 
@@ -40,21 +76,28 @@ public class MBStyleSpriteGenerator {
 
         // Create an instance of the SVG Generator.
         SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
-
+        svgGenerator.setPaint(Color.GRAY);
 
         AffineTransform at = new AffineTransform();
-
-        at.scale(24, 24);
+        at.scale(SCALE, SCALE);
         at.translate(0.5, 0.5);
 
-        svgGenerator.setSVGCanvasSize(new Dimension(24, 24));
+        if (rotation != null) {
+            at.rotate(rotation);
+        }
+
+        svgGenerator.setSVGCanvasSize(new Dimension(SCALE, SCALE));
+
         svgGenerator.fill(at.createTransformedShape(shape));
+        svgGenerator.setStroke(new BasicStroke(1));
+        svgGenerator.setColor(Color.BLACK);
+        svgGenerator.draw(at.createTransformedShape(shape));
 
-
-        //Write to file
-        Writer out = new FileWriter(filename);
+        // Write to file
+        Writer out = new FileWriter(Paths.get(OUTPUT_FOLDER.toString(), filename).toString());
         svgGenerator.stream(out, true);
         out.flush();
         out.close();
     }
+
 }
